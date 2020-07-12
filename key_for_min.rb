df4f15a5ee6bad64dd31e54d214e143317be7a2c
Dani Bassett <dani@danithetallgirl.com>
2020-07-12 06:39:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+ikea = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(ikea)
   lowest_key = nil
@@ -13,3 +14,5 @@ def key_for_min_value(ikea)
   end
   lowest_key
 end
+
+key_for_min_value(ikea)
