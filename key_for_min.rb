490cf7d91b57587773ba9108d1d0366406e67adb
Dani Bassett <dani@danithetallgirl.com>
2020-07-12 06:39:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,10 @@ ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(ikea)
   lowest_key = nil
   lowest_value = Float::INFINITY
-  hash.each do |item, value|
-    if value < lowest_value
-      lowest_value = value
-      lowest_key = item
+  hash.each do |k, v|
+    if v < lowest_value
+      lowest_value = v
+      lowest_key = k
     end
   end
   lowest_key
